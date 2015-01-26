5789fe3e929337814873772825685f4c6d6267f3
Nicolas Arciniega <nick7650@hotmail.com>
2015-01-25 21:45:00 -0500

    Functional but slow image drawing function

--- nicolas/drawMap.py
+++ nicolas/drawMap.py
@@ -1,11 +1,206 @@
-from nicolas.serverFunctions import BuildJsonResponse
-from nicolas.models import Map
+from nicolas.serverFunctions import BuildJsonResponse, IsOperationSuccess, GetRobotId, GetSpecificRobot, MapRobotDBToRobotObj
+from nicolas.dataObjects import MetaData, CellState
+from nicolas.models import Map, Waypoint
+import array
+import math
+import png
+import time
 
-# Used to draw visual map
+# Map colours
+UNEXPLORED_RGB = (0x88,0x88,0x88)
+EMPTY_RGB = (0xff,0xff,0xff)
+BLOCKED_RGB = (0x00,0x00,0x00)
+
+# Robot related colours
+TARGET_RGB = (0x33,0x33,0xff)
+ROBOT_EXACT_RGB = (0xcc,0x00,0x00)
+ROBOT_ESTIMATED_RGB = (0xff,0xb2,0x66)
+WAYPOINT_RGB = (0x99,0x00,0x99)
+WAYPOINT_REAL_RGB = (0x00,0xcc,0x00)
+
+# Other constants. Must scale by an integer factor
+NUM_LAYERS = 4
+SCALE_FACTOR = int(2)
+CHANNELS = 3
+FILE_PATH = 'nicolas/static/images/layer{layerNum}.png'
+
+def ConvertCoordinates(x, y):
+    """
+    Convert x and y coordinates in map to a coordinate in an image
+    """
+    if x is None or y is None:
+        raise Exception('x and y must have values')
+
+    xPos = x + MetaData.origin.x
+    yPos = MetaData.yMax - (y + MetaData.origin.y)
+
+    return CHANNELS * (yPos * MetaData.xMax + xPos)
+
+def CheckBounds(x, y):
+    """
+    Check if coordinates are in bounds of map
+    """
+    if x is None or y is None:
+        raise Exception('x and y must have values')
+
+    xPos = x + MetaData.origin.x
+    yPos = y + MetaData.origin.y
+
+    return (xPos >= 0 and xPos < MetaData.xMax and yPos >= 0 and yPos < MetaData.yMax)
+
+def PlusSign(x, y):
+    """
+    Form a plus sign centered on x, y
+    """
+    if x is None or y is None:
+        raise Exception('x and y must have values')
+
+    coordinates = []
+
+    if CheckBounds(x, y + 1):
+        coordinates.append(ConvertCoordinates(x, y + 1))
+
+    for i in range(-1, 2):
+        if CheckBounds(x + i, y):
+            coordinates.append(ConvertCoordinates(x + i, y))
+
+    if CheckBounds(x, y - 1):
+        coordinates.append(ConvertCoordinates(x, y - 1))
+
+    return coordinates
+
+def Square(x, y):
+    """
+    Form a square centered on x, y
+    """
+    if x is None or y is None:
+        raise Exception('x and y must have values')
+
+    coordinates = []
+
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            if CheckBounds(x + i, y + j):
+                coordinates.append(ConvertCoordinates(x + i, y + j))
+
+    return coordinates
 
 def VisualMapAction(params, body, method):
     """
     Use DB to draw out map and robots to 3 PNG files of sizes
     650x650, 1300x1300, and 2600x2600 (layer1, layer2, layer3)
     """
-    return BuildJsonResponse(False, 'Not Implemented')
+    # Create the map array for the base case
+    image = array.array('B', [0x00 for i in range(0, CHANNELS * MetaData.yMax * MetaData.xMax)])
+
+    idx = 0
+    px = 0
+    firstStart = time.clock()
+    mapDB = array.array('B', Map.objects.order_by('-y', 'x').values_list('state', flat=True))
+    for yPos in range(0, MetaData.yMax):
+        for xPos in range(0, MetaData.xMax):
+            pixel = mapDB[px]
+            for i in range(0, CHANNELS):
+                if pixel == CellState.unexplored:
+                    image[idx] = UNEXPLORED_RGB[i]
+                elif pixel == CellState.empty:
+                    image[idx] = EMPTY_RGB[i]
+                elif pixel == CellState.blocked:
+                    image[idx] = BLOCKED_RGB[i]
+                else:
+                    image[idx] = UNEXPLORED_RGB[i]
+                idx += 1
+            px += 1
+
+    # Replace pixels for robot position and waypoints
+    robotId = GetRobotId(params)
+    if robotId is not None:
+        result = GetSpecificRobot(robotId)
+        if not IsOperationSuccess(result):
+            return result
+
+        waypoints = Waypoint.objects.filter(robotId = robotId, realWaypoint = False)
+        robot = MapRobotDBToRobotObj(result['robot'], waypoints)
+
+        # Waypoints and connecting dots, if any
+        if robot.WaypointsExist():
+            for waypoint in robot.waypoints:
+                shape = Square(waypoint[0], waypoint[1])
+                for coord in shape:
+                    for i in range(0, CHANNELS):
+                        image[coord + i] = WAYPOINT_RGB[i]
+
+            # TODO: Now that the waypoints are drawn, connect the dots
+
+
+        # TODO: Real waypoints
+
+        # Robot target, if any
+        if robot.InstructionExists():
+            shape = Square(robot.instruction.x, robot.instruction.y)
+            for coord in shape:
+                for i in range(0, CHANNELS):
+                    image[coord + i] = TARGET_RGB[i]
+
+        # Robot estimated position
+        shape = PlusSign(robot.position.x, robot.position.y)
+        for coord in shape:
+            for i in range(0, CHANNELS):
+                image[coord + i] = ROBOT_ESTIMATED_RGB[i]
+
+        # Robot exact position
+        shape = PlusSign(robot.exactPosition.x, robot.exactPosition.y)
+        for coord in shape:
+            for i in range(0, CHANNELS):
+                image[coord + i] = ROBOT_EXACT_RGB[i]
+
+    # Draw the map and save as layer1.png
+    f = open(FILE_PATH.format(layerNum = 1), 'wb')
+    w = png.Writer(MetaData.xMax, MetaData.yMax)
+    w.write_array(f, image)
+    f.close()
+    firstEnd = time.clock()
+
+    # Create upscaled copies of original map
+    secondStart = time.clock()
+    width = MetaData.xMax
+    height = MetaData.yMax
+    pixels = width * height
+    layerNum = 2
+    scaleMatrix = array.array('B', [0x00 for i in range(0, SCALE_FACTOR * SCALE_FACTOR) ])
+    while layerNum <= NUM_LAYERS:
+        # Preserve previoius data
+        oldImage = image
+        oldPixels = pixels
+        oldWidth = width
+
+        # Initialize new values
+        width *= SCALE_FACTOR
+        height *= SCALE_FACTOR
+        pixels = width * height
+        image = array.array('B', [0x00 for i in range(0, CHANNELS * pixels)])
+
+        # Perform copy
+        for i in range(0, oldPixels):
+            rowNum = int(i / oldWidth)
+            colNum = i % oldWidth
+            pos = SCALE_FACTOR * ((rowNum * width) + colNum)
+            for xPos in range(0, SCALE_FACTOR):
+                for yPos in range(0, SCALE_FACTOR):
+                    for channel in range(0, CHANNELS):
+                        image[CHANNELS * (pos + xPos + (yPos * width)) + channel] = oldImage[(i * CHANNELS) + channel]
+
+        # Draw new map
+        f = open(FILE_PATH.format(layerNum = layerNum), 'wb')
+        w = png.Writer(width, height)
+        w.write_array(f, image)
+        f.close()
+
+        # Increment variables
+        layerNum += 1
+    secondEnd = time.clock()
+
+    print 'First part: ' + str(firstEnd - firstStart)
+    print 'Second part: ' + str(secondEnd - secondStart)
+
+    return BuildJsonResponse(True, 'Successfully created images')
